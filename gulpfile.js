ec690eba135840d2e80e7d026b5261b2238c26f4
EC2 Default User <ec2-user@ip-172-31-44-44.ec2.internal>
2018-10-10 21:30:09 +0000

    Added gulp-sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 var gulp = require('gulp'),
     uglify = require('gulp-uglify');
-
+var sass = require('gulp-sass');
 
 //Scripts Task
 //Uglifies
@@ -9,10 +9,22 @@ gulp.task('scripts',function() {
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'));
 });
+
+//Sass Task
+gulp.task('sass', function () {
+  return gulp.src('source/scss/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('dist/styles'));
+});
+gulp.task('sass:watch', function () {
+  gulp.watch('source/scss/*.scss', ['sass']);
+});
+
 //Styles Task
 
 gulp.task('styles',function() {
   console.log('Run Styles')
 });
 
-gulp.task ('default',['scripts','styles']);
+//default runs everthing
+gulp.task ('default',['scripts','styles', 'sass']);
